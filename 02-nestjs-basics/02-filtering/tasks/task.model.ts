32adadca171f021d62f5a2c170ff703dad946148
Viacheslav Gurov <v-gurov@yandex.ru>
2025-06-05 01:02:00 +0300

    add filter and sorting

--- 02-nestjs-basics/02-filtering/tasks/task.model.ts
+++ 02-nestjs-basics/02-filtering/tasks/task.model.ts
@@ -4,6 +4,11 @@ export enum TaskStatus {
   COMPLETED = "completed",
 }
 
+export enum SortColumns {
+  TITLE = "title",
+  STATUS = "status",
+}
+
 export interface Task {
   id?: string;
   title: string;
